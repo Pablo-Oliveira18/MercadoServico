f6b1160bd94a283c2566401bac9e11b0c29440de
Pablo Oliveira <pablogui1611@gmail.com>
2020-03-30 19:56:55 -0300

    Inclusão do Crud, e comunicação com o banco da entidade Servico

---
+++ src/java/br/com/mercadoservicos/dao/ServicoDao.java
@@ -0,0 +1,84 @@
+package br.com.mercadoservicos.dao;
+
+import br.com.mercadoservicos.domain.Servico;
+import br.com.mercadoservicos.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+
+
+public class ServicoDao {
+    
+    public List<Servico> listar() {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try {
+            List<Servico> servicos = session.createQuery("from Servico order by descricao").list();
+            session.getTransaction().commit();
+            return servicos;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+            return null;
+        }
+    }
+
+    public Servico consulta(Integer id) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try {
+            Servico servico = (Servico) session.createQuery("from Servico where id = " + id).uniqueResult(); // uniqueResult retorna apenas 1 resultado
+            session.getTransaction().commit();
+            return servico;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+            return null;
+        }
+    }
+
+    public boolean inserir(Servico servico) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try {
+            session.save(servico);
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+            return false;
+        }
+
+    }
+
+    public boolean excluir(Servico servico) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try {
+            session.delete(servico);
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+            return false;
+        }
+
+    }
+
+    public boolean alterar(Servico servico) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try {
+            session.update(servico);
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+            return false;
+        }
+
+    }
+    
+}
--- src/java/br/com/mercadoservicos/domain/Servico.java
+++ src/java/br/com/mercadoservicos/domain/Servico.java
@@ -1,50 +1,46 @@
 package br.com.mercadoservicos.domain;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-@Entity // tenta encotrar uma tabela no banco em um bjeto java
-@Table(name = "categoria")
-public class Categoria implements Serializable{
+@Entity
+@Table (name = "servicos")
+public class Servico implements Serializable{
     
-    @Id  // informa qual a chave primaria do banco >>> deve colocar em cima da msma...
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Especifica do MY SQL, dizendo que o ID é grado automaticamente...
-    
+    @Id // chave primaria
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     
-    @NotNull // fala q no banco ñ pode ser null
-    @Size (min = 1 , max = 45) // minimo de caracteres e o minimo.
-    @Column (name = "descricao") // qndo minha coluna tem outro nome
+    @NotNull
+    @Column(name="descricao")
     private String descricao;
     
-    @OneToMany // Uma categoria para muitos servicos
-    private List<Servico> servicos;
+    @NotNull
+    @Column(name = "preco")
+    private Double preco;
     
-    public Categoria() {
+    @ManyToOne // tenho um uúnico serviço para varias categorias
+    @JoinColumn (name = "idCategoria", referencedColumnName="id" ) // em qual coluna é feito essa junção, e nome da coluna q esta usando para referenciar
+    private Categoria categoria;
+    
+    public Servico(){
         
     }
 
-    public List<Servico> getServicos() {
-        return servicos;
-    }
-
-    public void setServicos(List<Servico> servicos) {
-        this.servicos = servicos;
-    }
-
-    public Categoria(Integer id, String descricao) {
+    public Servico(Integer id, String descricao, Double preco, Categoria categoria) {
         this.id = id;
         this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
     }
 
     public Integer getId() {
@@ -62,6 +58,25 @@ public class Categoria implements Serializable{
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+
+
 
     @Override
     public int hashCode() {
@@ -81,7 +96,7 @@ public class Categoria implements Serializable{
         if (getClass() != obj.getClass()) {
             return false;
         }
-        final Categoria other = (Categoria) obj;
+        final Servico other = (Servico) obj;
         if (!Objects.equals(this.id, other.id)) {
             return false;
         }
@@ -90,6 +105,10 @@ public class Categoria implements Serializable{
 
     @Override
     public String toString() {
-        return "Categoria{" + "id=" + id + ", descricao=" + descricao + '}';
-    } 
+        return "Servico{" + "id=" + id + ", descricao=" + descricao + ", preco=" + preco + ", categoria=" + categoria + '}';
+    }
+
+    
+    
+    
 }
